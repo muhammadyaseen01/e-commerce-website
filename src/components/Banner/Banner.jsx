22d65b4c01601705ae10b531bb0df125aebad5a4
Muhammad Yaseen <hafizyaseen.az@gmail.com>
2025-08-08 11:34:39 +0500

    changed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import BannerImage from "../../assets/Banner.jpg"
+import React from "react";
+import BannerImage from "../../assets/banner.jpg";
 function Banner() {
   return (
-    <section className='h-[60vh] mt-[14vh] bg-cover bg-top'
-    style={{backgroundImage : `url(${BannerImage})`}}
+    <section
+      className="h-[60vh] mt-[14vh] bg-cover bg-top"
+      style={{ backgroundImage: `url(${BannerImage})` }}
     >
-        <div className='max-w-[1300px] mx-auto px-12 h-100 flex flex-col justify-center gap-3'>
-            <h1 className='text-red-600 text-9xl uppercase font-bold tracking-tight'>Big Sale!</h1>
-            <h2 className='text-3xl text-zinc-800'>Up to 50% OFF - Limited Time Only!</h2>
-            <div className='text-6xl font-bold text-zinc-800 flex gap-x-3 mt-5 '>
-                <span className='text-white bg-zinc-800 p-3'>00</span>:
-                <span className='text-white bg-zinc-800 p-3'>00</span>:
-                <span className='text-white bg-zinc-800 p-3'>00</span>
-            </div>
+      <div className="max-w-[1300px] mx-auto px-12 h-100 flex flex-col justify-center gap-3">
+        <h1 className="text-red-600 text-9xl uppercase font-bold tracking-tight">
+          Big Sale!
+        </h1>
+        <h2 className="text-3xl text-zinc-800">
+          Up to 50% OFF - Limited Time Only!
+        </h2>
+        <div className="text-6xl font-bold text-zinc-800 flex gap-x-3 mt-5 ">
+          <span className="text-white bg-zinc-800 p-3">00</span>:
+          <span className="text-white bg-zinc-800 p-3">00</span>:
+          <span className="text-white bg-zinc-800 p-3">00</span>
         </div>
-        
+      </div>
     </section>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
 
-// 
-// 
+//
+//
